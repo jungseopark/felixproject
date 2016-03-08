9a288197868989ed9e28932b8c6ef36af023ff85
jungseo <chemj@mju.ac.kr>
2016-03-08 11:31:14 +0900

    database 컬럼 반영

--- src/main/java/kr/pe/felixproject/common/service/ProgramVO.java
+++ src/main/java/kr/pe/felixproject/common/service/ProgramVO.java
@@ -10,6 +10,14 @@ public class ProgramVO implements Serializable {
 
 	private String searchName;
 
+	private String programId;
+
+	private String name;
+
+	private String description;
+
+	private String url;
+
 	public String toString() {
 		return ToStringBuilder.reflectionToString(this);
 	}
@@ -20,6 +28,38 @@ public class ProgramVO implements Serializable {
 
 	public void setSearchName(String searchName) {
 		this.searchName = searchName;
+	}
+
+	public String getProgramId() {
+		return programId;
+	}
+
+	public void setProgramId(String programId) {
+		this.programId = programId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 }
