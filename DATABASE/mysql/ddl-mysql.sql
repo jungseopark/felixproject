bce7d83b0900eabeff6495469a3fd4349ee05509
jungseo <chemj@mju.ac.kr>
2016-03-18 09:38:07 +0900

    초기 개발중

--- DATABASE/mysql/ddl-mysql.sql
+++ DATABASE/mysql/ddl-mysql.sql
@@ -1,34 +1,30 @@
-/* Sql Log */
-create table TN_SQLLOG (
+create table TN_SQL_LOG (
 	LOG_SEQ  varchar(20) not null,
-	LOG_LEVEL varchar(5) null,
-	LOGGER varchar(100) null,
-	QUERY text null,
-	STAFF_NO varchar(20) null,
-	IPADDRESS varchar(15) null,
-	primary key(LOG_SEQ)
-);
-
-/* Method Log */
-create table TN_METHODLOG (
-	LOG_SEQ varchar(20) not null,
-	CLASS_NAME varchar(100),
-	METHOD_NAME varchar(100),
+	LOG_LEVEL varchar(5),
+	LOGGER varchar(100),
+	LOG_QUERY text not null,
 	STAFF_NO varchar(20),
-	IPADDRESS varchar(15),
+	IP_ADDRESS varchar(15) not null,
 	primary key(LOG_SEQ)
 );
 
-/* Web Log */
-create table TN_WEBLOG (
+create table TN_METHOD_LOG (
 	LOG_SEQ varchar(20) not null,
-	URL varchar(200),
+	CLASS_NAME varchar(100) not null,
+	METHOD_NAME varchar(100) not null,
 	STAFF_NO varchar(20),
-	IPADDRESS varchar(15),
+	IP_ADDRESS varchar(15) not null,
 	primary key(LOG_SEQ)
 );
 
-/* 로그인 사용자 */
+create table TN_WEB_LOG (
+	LOG_SEQ varchar(20) not null,
+	URL varchar(200) not null,
+	STAFF_NO varchar(20),
+	IP_ADDRESS varchar(15) not null,
+	primary key(LOG_SEQ)
+);
+
 create table TN_USER (
 	USER_ID varchar(20) not null,
 	STAFF_NO varchar(20) not null,
@@ -37,16 +33,14 @@ create table TN_USER (
 	primary key(USER_ID)
 );
 
-/* 프로그램 */
-create table TN_PROGRAM (
+create table TN_PROGRAM_INFO (
 	PROGRAM_ID varchar(50) not null,
 	PROGRAM_NAME varchar(100) not null,
-	DESCRIPTION varchar(200) not null,
-	URL varchar(200) not null,
+	DESCRIPTION varchar(200),
+	PROGRAM_URL varchar(200) not null,
 	primary key(PROGRAM_ID)
 );
 
-/* 권한 */
 create table TN_ROLE (
 	ROLE_ID varchar(50) not null,
 	ROLE_NAME varchar(100) not null,
@@ -54,16 +48,17 @@ create table TN_ROLE (
 	primary key(ROLE_ID)
 );
 
-/* 권한별 프로그램 */
 create table TN_PROGRAM_ROLE (
 	ROLE_ID varchar(50) not null,
 	PROGRAM_ID varchar(50) not null
 	primary key(ROLE_ID, PROGRAM_ID)
 );
 
-/* 사용자별 권한 */
 create table TN_USER_ROLE (
 	USER_ID varchar(20) not null,
 	ROLE_ID varchar(50) not null
 	primary key(USER_ID, ROLE_ID)
+);
+
+create table TN_MENU (
 );
