f1938ad4980017a5fb337c811501e918861ba938
jungseo <chemj@mju.ac.kr>
2016-03-30 16:20:23 +0900

    국가코드 관리 컨트롤러 작성중

--- src/main/java/name/felix/commons/code/web/NationCodeManageController.java
+++ src/main/java/name/felix/commons/code/web/NationCodeManageController.java
@@ -1,8 +1,40 @@
 package name.felix.commons.code.web;
 
+import javax.annotation.Resource;
+
+import name.felix.commons.code.service.NationCodeManageService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 
 @Controller
 public class NationCodeManageController {
+
+	private static Logger logger = LoggerFactory.getLogger(NationCodeManageController.class);
+
+	@Resource(name = "NationCodeManageService")
+	NationCodeManageService nationCodeManageService;
+	
+	public String selectNationCodeList(ModelMap model) throws Exception {
+		return null;
+	}
+	
+	public String selectNationCodeDetail(ModelMap model) throws Exception {
+		return null;
+	}
+	
+	public String insertNationCode(ModelMap model) throws Exception {
+		return null;
+	}
+	
+	public String updateNationCode(ModelMap model) throws Exception {
+		return null;
+	}
+	
+	public String deleteNationCode(ModelMap model) throws Exception {
+		return null;
+	}
 
 }
