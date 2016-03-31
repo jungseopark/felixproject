38bf9f3027b44d30cda4ed6678b258d85ba69063
jungseo <chemj@mju.ac.kr>
2016-03-31 16:27:17 +0900

    return jsp 정의

--- src/main/java/name/felix/commons/code/web/NationCodeManageController.java
+++ src/main/java/name/felix/commons/code/web/NationCodeManageController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NationCodeManageController {
@@ -16,25 +17,30 @@ public class NationCodeManageController {
 
 	@Resource(name = "NationCodeManageService")
 	NationCodeManageService nationCodeManageService;
-	
+
+	@RequestMapping(value = "/commons/code/NationCodeList.do")
 	public String selectNationCodeList(ModelMap model) throws Exception {
-		return null;
+		return "commons/code/NationCodeList";
 	}
-	
+
+	@RequestMapping(value = "/commons/code/NationCodeDetail.do")
 	public String selectNationCodeDetail(ModelMap model) throws Exception {
-		return null;
+		return "commons/code/NationCodeDetail";
 	}
-	
+
+	@RequestMapping(value = "/commons/code/NationCodeRegist.do")
 	public String insertNationCode(ModelMap model) throws Exception {
-		return null;
+		return "forward:/commons/NationCodeDetail.do";
 	}
-	
+
+	@RequestMapping(value = "/commons/code/NationCodeModify.do")
 	public String updateNationCode(ModelMap model) throws Exception {
-		return null;
+		return "forward:/common/code/NationCodeDetail.do";
 	}
-	
+
+	@RequestMapping(value = "/commons/code/NationCodeRemove.do")
 	public String deleteNationCode(ModelMap model) throws Exception {
-		return null;
+		return "forward:/commons/code/NationCodeList.do";
 	}
 
 }
